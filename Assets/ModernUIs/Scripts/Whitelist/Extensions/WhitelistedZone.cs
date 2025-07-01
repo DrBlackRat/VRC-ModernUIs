6b2744be3be28c64969b0d9d566fff7e04e52905
DrBlackRat <contact@drblackrat.xyz>
2025-07-02 00:27:35 +0200

    Added WhitelistedZone
    - Added Whitelisted Zone
      - Defines an area in which only players who are whitelisted can be in
      - If they aren't whitelisted and enter / are in the trigger they will be teleported to the exit position

---
+++ Assets/ModernUIs/Scripts/Whitelist/Extensions/WhitelistedZone.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using DrBlackRat.VRC.ModernUIs.Whitelist.Base;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace DrBlackRat.VRC.ModernUIs.Whitelist
+{
+    public class WhitelistedZone : UdonSharpBehaviour
+    {
+        [SerializeField] private WhitelistGetterBase whitelist;
+        [SerializeField] private Transform exitTransform;
+
+        private Collider zoneCollider;
+        
+        private bool hasAccess;
+        private bool inZone;
+        
+        private void Start()
+        {
+            zoneCollider = GetComponent<Collider>();
+            whitelist._SetUpConnection(GetComponent<UdonBehaviour>());
+        }
+
+        public void _WhitelistUpdated()
+        {
+            hasAccess = whitelist._IsPlayerWhitelisted(Networking.LocalPlayer);
+            CheckAccess();
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (!player.isLocal || inZone) return;
+            inZone = true;
+            CheckAccess();
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            inZone = false;
+        }
+
+        private void CheckAccess()
+        {
+            if (inZone && !hasAccess)
+            {
+                Networking.LocalPlayer.TeleportTo(exitTransform.position, exitTransform.rotation);
+            }
+        }
+        
+        // Spawn / Respawn Fixes
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            zoneCollider.enabled = false;
+            SendCustomEventDelayedFrames(nameof(_EnableCollider), 0);
+        }
+        public override void OnPlayerRespawn(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            zoneCollider.enabled = false;
+            SendCustomEventDelayedFrames(nameof(_EnableCollider), 0);
+        }
+        public void _EnableCollider()
+        {
+            zoneCollider.enabled = true;
+        }
+    }
+}
+
