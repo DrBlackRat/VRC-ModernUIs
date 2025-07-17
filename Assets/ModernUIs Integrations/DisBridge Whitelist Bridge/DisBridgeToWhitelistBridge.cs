da33d2ab12523020ab25c4ff3554f469a24ebbc7
DrBlackRat <contact@drblackrat.xyz>
2025-07-17 17:48:05 +0200

    Added Whitelist and Synced Prefabs

---
+++ Assets/ModernUIs Integrations/DisBridge Whitelist Bridge/DisBridgeToWhitelistBridge.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UdonVR.DisBridge;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace DrBlackRat.VRC.ModernUIs.Integrations
+{
+    public class DisBridgeToWhitelistBridge : DisBridgePlugin
+    {
+        private void Start()
+        {
+            disBridge.AddPlugin(gameObject);
+        }
+
+        //Runs when DisBridge finishes pulling the roles.
+        public override void _UVR_Init()
+        {
+
+        }
+        
+        //Runs when a RoleContainer's user list has updated.
+        public override void _UVR_Update()
+        {
+            var supporters = disBridge.GetSupporterDisplaynames();
+        }
+    }
+}
